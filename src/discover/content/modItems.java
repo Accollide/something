0c772f4e49e880c32de2699165e3457ad78e4294
EchoSabre2 <164820125+EchoSabre2@users.noreply.github.com>
2024-04-05 15:19:01 -0700

    stuff

---
+++ src/discover/content/modItems.java
@@ -0,0 +1,30 @@
+package biotech.content;
+
+import arc.graphics.Color;
+import arc.struct.Seq;
+import mindustry.type.Item;
+
+public class BioItems {
+    public static Item
+        test
+        
+
+    public static final Seq<Item> moditems = new Seq<>();
+    public static void load() {
+        test = new Item("test",Color.valueOf("ffffff")) {{
+            hardness = 1;
+            cost = 0.7f;
+            alwaysUnlocked = true;
+        }}
+
+        
+
+        
+
+        
+
+        moditems.addAll(
+                test
+        );
+    }
+}
